a9a2fe5977aabab8f17d07154abf0f9de1a6bb1d
hugolawrens <hugolawrens40@gmail.com>
2024-04-01 14:10:42 +0700

    add final project files with video

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-  video: false,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
